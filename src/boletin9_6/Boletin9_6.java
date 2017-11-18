cece429ed19ccd49f87e5cb889230f02800d1752
Sauky <Sauky@DESKTOP-2HUBGV7>
2017-11-18 14:53:23 +0100

    Codigo Final

---
+++ src/boletin9_6/Boletin9_6.java
@@ -0,0 +1,12 @@
+package boletin9_6;
+
+public class Boletin9_6 {
+
+    public static void main(String[] args) {
+        //Creamos un objeto de la clase sueldos
+        Sueldos S1 = new Sueldos();
+        //Llamamos al método
+        S1.mostrarSueldos();
+    }
+    
+}
---
+++ src/boletin9_6/Sueldos.java
@@ -0,0 +1,30 @@
+package boletin9_6;
+import java.util.Scanner;
+public class Sueldos {
+    Scanner sc = new Scanner(System.in);
+
+    //Instanciamos el método
+    public void mostrarSueldos(){
+        //instanciamos las variables e inicializamos i y j
+        int i=0, j=0;
+        float sueldo;
+        //creamos la estructura repetitiva
+        do{
+            System.out.println("Introduza el sueldo");
+            sueldo = sc.nextFloat();
+            if(sueldo>=1000 && sueldo<=1750){
+                i++;
+            }
+            else if (sueldo<1000 && sueldo>0){
+                j++;
+            }
+            else{
+                System.out.println("Cantidad errónea");
+            }
+        }while(sueldo!=0);
+    //mostramos los resultados
+        System.out.println("Trabajadores con sueldo entre 1000€ y 1750€: "+i);
+        System.out.println("Trabajadores con sueldo inferior a 1000€: "+j);
+    }
+    
+}
